9955a72567e14d45de2c24d153bfc462539e054d
Ben Civjan <bcivjan2@illinois.edu>
2021-04-01 14:55:03 -0500

    Basic frontend layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1> Vaccine Form </h1>
+
+			<div className="form">
+				<label> First Name: </label>
+				<input type="text" name="firstName" />
+
+				<label> Last Name: </label>
+				<input type="text" name="lastName" />
+
+				<label> Vaccine Brand: </label>
+				<input type="text" name="brand" />
+
+				<button> Submit</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
