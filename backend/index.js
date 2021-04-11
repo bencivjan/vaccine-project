8f02663edf22143ad5dbe8510d26ca84c92c8c4f
Ben Civjan <bcivjan2@illinois.edu>
2021-04-10 19:31:49 -0500

    Working on connecting db

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,102 @@
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql");
+
+// var db = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "123abc",
+// 	database: "vaccination_database"
+// });
+
+// app.get("/", (require, response) => {
+// 	const sqlInsert = "SELECT * FROM vaccines";
+// 	db.query(sqlInsert, (err, result) => {
+// 		response.send("Hello world!");
+// 	});
+// });
+
+// app.listen(3001, () => {
+// 	console.log("running on port 3001");
+// });
+//
+//
+//
+//
+//
+//
+//
+
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
 
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123abc',
-    database: 'vaccination_database'
-})
+	host: "34.70.192.41",
+	user: "root",
+	password: "123abc",
+	database: "vaccine_db"
+});
 
-app.get('/', (require, response) => {
-    const sqlInsert = "SELECT * FROM vaccines";
-    db.query(sqlInsert, (err, result) => {
-        response.send("Hello world!");
-    })
-})
+// db.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "INSERT INTO `movie_reviews` (`id`,`movieName`, `movieReview`) VALUES (5,'inception', 'good movie');";
+//     db.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log(result.affectedRows + " record(s) updated");
+//     });
+//   });
 
-app.listen(3001, () => {
-    console.log("running on port 3001");
-})
+// app.get("/", (require, response) => {
+// 	const sqlInsert = "INSERT INTO `vaccines` (`vaccine_id`, `vaccine_name`, `vaccine_brand`) VALUES ('4', 'a', 'b');";
+// 	db.query(sqlInsert, (err, result) => {
+// 		response.send("Hello world!!!");
+// 	});
+// });
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/get", (require, response) => {
+	const sqlSelect = "SELECT * FROM movie_reviews";
+	db.query(sqlSelect, (err, result) => {
+		response.send(result);
+	});
+});
+
+app.post("/api/insert", (require, response) => {
+	const vaccine_id = require.body.vaccine_id;
+	const vaccine_name = require.body.vaccine_name;
+	const vaccine_brand = require.body.vaccine_brand;
+
+	const sqlInsert = "INSERT INTO `vaccines` (`vaccine_id`, `vaccine_name`, `vaccine_brand`) VALUES (?,?, ?)";
+	db.query(sqlInsert, [ vaccine_id, vaccine_name, vaccine_brand ], (err, result) => {
+		console.log(error);
+	});
+});
+
+app.delete("/api/delete/:movieName", (require, response) => {
+	const movieName = require.params.movieName;
+
+	const sqlDelete = "DELETE FROM `movie_reviews` WHERE `movieName`= ?";
+	db.query(sqlDelete, movieName, (err, result) => {
+		if (err) console.log(error);
+	});
+});
+
+app.put("/api/update/", (require, response) => {
+	const movieName = require.body.movieName;
+	const movieReview = require.body.movieReview;
+
+	const sqlUpdate = "UPDATE `movie_reviews` SET `movieReview` = ? WHERE `movieName`= ?";
+	db.query(sqlUpdate, [ movieReview, movieName ], (err, result) => {
+		if (err) console.log(error);
+	});
+});
+
+app.listen(3002, () => {
+	console.log("running on port 3002");
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,144 @@
+// import "./App.css";
+// import Axios from "axios";
+// import React, { useState, useEffect } from "react";
+
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<h1> Vaccine Form </h1>
+
+// 			<div className="form">
+// 				<label> First Name: </label>
+// 				<input type="text" name="firstName" />
+
+// 				<label> Last Name: </label>
+// 				<input type="text" name="lastName" />
+
+// 				<label> Vaccine Brand: </label>
+// 				<input type="text" name="brand" />
+
+// 				<button> Submit</button>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default App;
 import "./App.css";
-import Axios from "axios";
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+	const [ movieName, setMovieName ] = useState("");
+	const [ Review, setReview ] = useState("");
+	const [ Brand, setBrand ] = useState("");
+
+	const [ movieReviewList, setMovieReviewList ] = useState([]);
+	const [ newReview, setNewReview ] = useState("");
+
+	useEffect(() => {
+		Axios.get("http://localhost:3002/api/get").then(response => {
+			setMovieReviewList(response.data);
+		});
+	}, []);
+
+	const submitReview = () => {
+		console.log("Submitted");
+		console.log(movieName);
+		console.log(Review);
+		console.log(Brand);
+		Axios.post("http://localhost:3002/api/insert", {
+			vaccine_id: movieName,
+			vaccine_name: Review,
+			vaccine_brand: Brand
+		});
+
+		setMovieReviewList([
+			...movieReviewList,
+			{
+				vaccine_id: movieName,
+				vaccine_name: Review,
+				vaccine_brand: Brand
+			}
+		]);
+	};
+
+	const deleteReview = movieName => {
+		Axios.delete(`http://localhost:3002/api/delete/${movieName}`);
+	};
+
+	const updateReview = movieName => {
+		Axios.put(`http://localhost:3002/api/update`, {
+			movieName: movieName,
+			movieReview: newReview
+		});
+		setNewReview("");
+	};
+
 	return (
 		<div className="App">
-			<h1> Vaccine Form </h1>
+			<h1> CRUD APPLICATIONS</h1>
 
 			<div className="form">
-				<label> First Name: </label>
-				<input type="text" name="firstName" />
-
-				<label> Last Name: </label>
-				<input type="text" name="lastName" />
+				<label> Movie Name:</label>
+				<input
+					type="text"
+					name="movieName"
+					onChange={e => {
+						setMovieName(e.target.value);
+					}}
+				/>
+				<label> Review:</label>
+				<input
+					type="text"
+					name="Review"
+					onChange={e => {
+						setReview(e.target.value);
+					}}
+				/>
 
 				<label> Vaccine Brand: </label>
-				<input type="text" name="brand" />
+				<input
+					type="text"
+					name="Brand"
+					onChange={e => {
+						setReview(e.target.value);
+					}}
+				/>
 
-				<button> Submit</button>
+				<button onClick={submitReview}> Submit</button>
+
+				{movieReviewList.map(val => {
+					return (
+						<div className="card">
+							<h1> MovieName: {val.movieName} </h1>
+							<p>Movie Review: {val.movieReview}</p>
+							<button
+								onClick={() => {
+									deleteReview(val.movieName);
+								}}
+							>
+								{" "}
+								Delete
+							</button>
+							<input
+								type="text"
+								id="updateInput"
+								onChange={e => {
+									setNewReview(e.target.value);
+								}}
+							/>
+							<button
+								onClick={() => {
+									updateReview(val.movieName);
+								}}
+							>
+								{" "}
+								Update
+							</button>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
