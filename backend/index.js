80da621e2d84ba98aff1bd2bcd2b4c762da0eefc
Ben Civjan <bcivjan2@illinois.edu>
2021-04-26 21:15:55 -0500

    Set up create user route

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,42 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.post("/api/createUser", (require, response) => {
+	const username = require.body.username;
+	const password = require.body.password;
+	const first = require.body.first;
+	const last = require.body.last;
+	const age = require.body.age;
+	const email = require.body.email;
+
+	const getLastId = "SELECT max(person_id) as id from person";
+
+	const personInsert =
+		"INSERT INTO `person` (`person_id`, `first_name`, `last_name`, `age`, `email`) VALUES (?,?,?,?,?)";
+	const loginInsert = "INSERT INTO `user_login` (`username`, `password`) VALUES (?,?)";
+	const aiInsert = "INSERT INTO `account_information` (`username`, `person_id`) VALUES (?,?)";
+
+	db.query(getLastId, (err, result) => {
+		const person_id = result.id + 1;
+
+		db.query(personInsert, [ person_id, first, last, age, email ], (errPerson, resultPerson) => {
+			console.log(resultPerson);
+			console.log(errPerson);
+		});
+
+		db.query(loginInsert, [ username, password ], (errLogin, resultLogin) => {
+			console.log(resultLogin);
+			console.log(errLogin);
+		});
+
+		db.query(aiInsert, [ username, person_id ], (errAI, resultAI) => {
+			console.log(resultAI);
+			console.log(errAI);
+		});
+	});
+});
+
+// ================= STAGE 4 CODE BELOW========================
 app.get("/api/initdata", (require, response) => {
 	const sqlSelect = "SELECT * FROM vaccines";
 	db.query(sqlSelect, (err, result) => {
@@ -27,7 +63,7 @@ app.post("/api/search", (require, response) => {
 	const search_query = require.body.search_query;
 	const sqlSearch = "SELECT * FROM `vaccines` WHERE `vaccine_id` = ? OR `vaccine_name` =  ? OR `vaccine_brand` = ?";
 
-	db.query(sqlSearch, [search_query, search_query, search_query], (err, result) => {
+	db.query(sqlSearch, [ search_query, search_query, search_query ], (err, result) => {
 		response.send(result);
 		console.log(err);
 	});
@@ -39,7 +75,7 @@ app.post("/api/insert", (require, response) => {
 	const vaccine_brand = require.body.vaccine_brand;
 
 	const sqlInsert = "INSERT INTO `vaccines` (`vaccine_id`, `vaccine_name`, `vaccine_brand`) VALUES (?,?,?)";
-	db.query(sqlInsert, [vaccine_id, vaccine_name, vaccine_brand], (err, result) => {
+	db.query(sqlInsert, [ vaccine_id, vaccine_name, vaccine_brand ], (err, result) => {
 		console.log(result);
 		console.log(err);
 	});
@@ -58,7 +94,7 @@ app.put("/api/update/", (require, response) => {
 	const vaccine_id = require.body.vaccine_id;
 	const vaccine_brand = require.body.vaccine_brand;
 	const sqlUpdate = "UPDATE `vaccines` SET `vaccine_brand` = ?  WHERE `vaccine_id`= ?";
-	db.query(sqlUpdate, [vaccine_brand, vaccine_id], (err, result) => {
+	db.query(sqlUpdate, [ vaccine_brand, vaccine_id ], (err, result) => {
 		console.log(result);
 		if (err) console.log(err);
 	});
@@ -69,7 +105,7 @@ app.get("/api/advanced_query", (require, response) => {
 	db.query(sqlInsert, (err, result) => {
 		console.log(result);
 		response.send(result);
-	})
+	});
 });
 
 app.listen(3002, () => {
